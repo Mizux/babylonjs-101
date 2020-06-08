992c2ee519c16eaacbe762ec5d26a51733751f22
Mizux Seiha <mizux.dev@gmail.com>
2020-06-08 02:16:13 +0200

    Update webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,16 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
 	devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+    contentBase: ['.'],
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    noInfo: true
+  },
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -30,11 +39,7 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
+    extensions: ['.ts', '.tsx', '.js']
   },
   performance: {
     hints: false
